3b76d6ff5761672b21eb6ef786f09759d99f8095
Johannes Krabbe <Johannes.Krabbe@GoKrabbe.de>
2022-04-20 11:15:19 +0200

    add docker for deployment

--- helpers/context.ts
+++ helpers/context.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const request = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: process.env.NODE_ENV === "production" ? "http://localhost:3001" : "https://api.petbook.johanneskrabbe.com",
   timeout: 1000,
   withCredentials: true,
 });
