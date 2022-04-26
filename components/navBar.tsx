565086da2e1465e71f58b12c1da546bb5f2d06a5
Johannes Krabbe <Johannes.Krabbe@GoKrabbe.de>
2022-04-26 17:02:24 +0200

    created register/ login/ logout flow

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -1,15 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { request } from "../helpers/context";
+import { deleteCookie } from "../helpers/cookieHelper";
+
+import Router from "next/router";
+
+import LogoutIcon from "@mui/icons-material/Logout";
+import { IconButton } from "@mui/material";
 
 interface NavBarProps {}
 
-export class NavBar extends React.Component<NavBarProps> {
-  render() {
-    return (
-      <div className="border-solid border-2 border-gray-700 bg-gray-800">
-        <div className="flex flex-col">
-          <p className="text-white text-3xl font-bold my-5 ml-40">PetBook</p>
-        </div>
-      </div>
-    );
-  }
-}
+export const NavBar = () => {
+	const [userName, setUserName] = useState("");
+
+	const logout = () => {
+		deleteCookie("pb_jwt");
+		Router.push("/");
+	};
+
+	useEffect(() => {
+		request
+			.get("/user/getMe")
+			.then((response) => {
+				if (response.status === 200) {
+					setUserName(response.data.username);
+				}
+			})
+			.catch((e) => {});
+	});
+
+	return (
+		<div className="border-solid border-2 border-gray-700 bg-gray-800">
+			<div className="flex flex-row w-full h-20 items-center">
+				<p className="text-white text-3xl font-bold ml-40 w-full">
+					<a href="/">PetBook</a>
+				</p>
+				<div className="flex flex-row-reverse">
+					{userName ? (
+						<div className="flex flex-row items-center text-white text-xl text-right font-bold mr-10">
+							<a className="mr-4" href="/me">
+								{userName}
+							</a>
+							<IconButton size="small" onClick={() => logout()}>
+								<LogoutIcon />
+							</IconButton>
+						</div>
+					) : (
+						<p className="text-white text-xl text-right font-bold mr-20">
+							<a href="/login"> Login </a>
+						</p>
+					)}
+				</div>
+			</div>
+		</div>
+	);
+};
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -2,77 +2,91 @@ import { TextField, Button } from "@mui/material";
 import { Formik } from "formik";
 import { request } from "../../helpers/context";
 import { NextPage } from "next";
-import React from "react";
+import React, { useEffect } from "react";
 import { NavBar } from "../../components/navBar";
+import Router from "next/router";
+import { getCookie } from "../../helpers/cookieHelper";
 
 const Login: NextPage = () => {
-  const [loginMessage, setLoginMessage] = React.useState("");
+	useEffect(() => {
+		if (getCookie("pb_jwt")) {
+			Router.push("/me");
+		}
+	});
 
-  const login = (username: string, password: string) => {
-    request
-      .post("/user/login", { username, password })
-      .then((response) => {
-        if (response.status === 200) {
-          document.cookie = `pb_jwt=${response.data.token};max-age=${
-            60 * 60 * 24 * 30
-          }`;
-          setLoginMessage("");
-        }
-      })
-      .catch((e) => {
-        setLoginMessage(e.response.data.message);
-      });
-  };
+	const [loginMessage, setLoginMessage] = React.useState("");
 
-  return (
-    <div>
-      <NavBar />
-      <div className="font-sans grid place-items-center h-screen bg-gray-900">
-        <Formik
-          initialValues={{ username: "", password: "" }}
-          onSubmit={(arg) => {
-            login(arg.username, arg.password);
-          }}
-        >
-          {({ handleSubmit, handleChange }) => (
-            <form
-              onSubmit={(e) => {
-                e.preventDefault();
-                handleSubmit(e);
-              }}
-            >
-              <div className="flex flex-col">
-                <div className="flex flex-col items-center">
-                  <div className="mb-3 items-center">
-                    <TextField
-                      name="username"
-                      label="Username"
-                      variant="outlined"
-                      onChange={handleChange}
-                    ></TextField>
-                  </div>
-                  <div className="mb-1 items-center">
-                    <TextField
-                      name="password"
-                      type="password"
-                      label="Password"
-                      variant="outlined"
-                      onChange={handleChange}
-                    ></TextField>
-                  </div>
-                </div>
-                <p className="h-5 mb-1 text-red-600 text-s text-center">
-                  {loginMessage}
-                </p>
-                <Button type="submit">Login</Button>
-                <div className="items-center"></div>
-              </div>
-            </form>
-          )}
-        </Formik>
-      </div>
-    </div>
-  );
+	const login = (username: string, password: string) => {
+		request
+			.post("/user/login", { username, password })
+			.then((response) => {
+				if (response.status === 200) {
+					document.cookie = `pb_jwt=${response.data.token};max-age=${
+						60 * 60 * 24 * 30
+					};path=/`;
+
+					setLoginMessage("");
+					Router.push("/me");
+				}
+			})
+			.catch((e) => {
+				setLoginMessage(e.response.data.message);
+			});
+	};
+
+	return (
+		<div>
+			<NavBar />
+			<div className="font-sans grid place-items-center h-screen bg-gray-900">
+				<Formik
+					initialValues={{ username: "", password: "" }}
+					onSubmit={(arg) => {
+						login(arg.username, arg.password);
+					}}
+				>
+					{({ handleSubmit, handleChange }) => (
+						<form
+							onSubmit={(e) => {
+								e.preventDefault();
+								handleSubmit(e);
+							}}
+						>
+							<div className="flex flex-col">
+								<div className="flex flex-col items-center">
+									<div className="mb-3 items-center">
+										<TextField
+											name="username"
+											label="Username"
+											variant="outlined"
+											onChange={handleChange}
+										></TextField>
+									</div>
+									<div className="mb-1 items-center">
+										<TextField
+											name="password"
+											type="password"
+											label="Password"
+											variant="outlined"
+											onChange={handleChange}
+										></TextField>
+									</div>
+								</div>
+								<p className="h-5 mb-1 text-red-600 text-s text-center">
+									{loginMessage}
+								</p>
+								<Button type="submit">Login</Button>
+								<p className="mt-3 text-gray-300 text-center">
+									<a href="/login/register">
+										Dont have an account? Register here
+									</a>
+								</p>
+							</div>
+						</form>
+					)}
+				</Formik>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
